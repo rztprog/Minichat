51d3b5cbcef2e72f674fb0cbc4f96659452717bd
rztprog <rztprog@outlook.fr>
2019-05-20 15:45:57 +0200

    add bulma & css

--- minichat.php
+++ minichat.php
@@ -2,20 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css" />
+	<link rel="stylesheet" type="text/css" href="./css/style.css"/>
     <title>Minichat PHP</title>
 </head>
-<body>
-    <div>    
-        <form class="form" action="minichat_post.php" method="post">
-        <h1>MINICHAT</h1>
-        <p><label for="">Nickname : </label>
-        <input type="text" name="pseudo" placeholder="Your nickname" required autofocus /></p>
+<body class="body">
+    <div class="form"> 
+        <form action="minichat_post.php" method="post">
+        <h1 class="bigtitle">MINICHAT</h1>
+        <p><label for="">Nickname : </label></p>
+        <p><input type="text" name="pseudo" placeholder="Your nickname" required autofocus /></p>
         <p><label for="">Message : </label>
-        <textarea name="message" rows="10" cols="40" maxlength="255" placeholder="Your message" required></textarea></p>
-        <p><input type="submit" value="Send" /></p>
+        <p><textarea class="message" name="message" rows="10" cols="40" maxlength="255" placeholder="Your message" required></textarea></p>
+        <p><input class="button is-success is-outlined" type="submit" value="Send" />
+        <button class="button is-link is-outlined" ><a href="minichat.php">Reload</a></button</p>
     </form>
     </div>
-    <div>
+    <div class="chatbox">
 <?php
 try
 {
