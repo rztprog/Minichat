eb640834225a54515d8cfc3ab3bdbf477e63b2b7
rztprog <rztprog@outlook.fr>
2019-05-22 12:05:09 +0200

    fix bug on first send

--- minichat.php
+++ minichat.php
@@ -5,6 +5,7 @@ try
     $bdd = new PDO('mysql:host=localhost;dbname=mini_chat;charset=utf8', 'root', '');
 }
 catch(Exception $e)
+
 {
     // En cas d'erreur, on affiche un message et on arrête tout
     die('Erreur : '.$e->getMessage());
@@ -17,27 +18,34 @@ catch(Exception $e)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Righteous&effect=3d" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css" />
 	<link rel="stylesheet" type="text/css" href="./css/style.css"/>
-    <title>Minichat PHP</title>
+    <title>SecureChat</title>
 </head>
 <body class="body">
     <div class="form"> 
         <form action="minichat_post.php" method="post">
-        <h1 class="bigtitle">MINICHAT</h1>
-        <p>
-        <p class="control has-icons-left">
-            <input type="text" name="pseudo" class="input is-medium is-primary is-rounded" placeholder="Your nickname" value="" required autofocus />
-            <span class="icon is-small is-left">
-                <i class="fas fa-cannabis"></i>
-            </span>
+        <h1 class="bigtitle">SECURECHAT</h1>
+        <p class="form">Your IP: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+        <p class="control">
+        <?php
+        if(!isset($_COOKIE['pseudo']))
+        {?>
+        <input type="text" name="pseudo" class="input is-medium is-primary" placeholder="Your nickname" value="" required />
+        <?php
+        }
+        else
+        {?>
+        <input type="text" name="pseudo" class="input is-medium is-primary" placeholder="Your nickname" value="<?= $_COOKIE['pseudo'] ?>" required />
+        <?php 
+        } 
+        ?>
         </p>
-        <p>
-        <p><textarea class="message textarea is-primary is-medium margintop" name="message" rows="2" cols="40" required maxlength="255" placeholder="Your message"></textarea></p>
-        <p class="margintop"><input class="button is-success" type="submit" value="Send" />
-        <input class="button is-success" type="reset" value="Reset" />
-        <button class="button is-success" value="ntm"><a class="is-black" href="minichat.php">Refresh</a></button>
+        <p class="control"><textarea class="message textarea is-primary is-medium margintop" name="message" rows="4" cols="40" required autofocus maxlength="255" placeholder="Your message"></textarea></p>
+        <p class="margintop"><input class="button is-medium is-success" type="submit" value="Send" />
+        <input class="button is-success is-medium" type="reset" value="Reset" />
+        <button class="button is-success is-medium" value="ntm"><a class="is-black" href="minichat.php">Refresh</a></button>
         <!-- <button class="button is-success">Clear</button> -->
         </p>
     </form>
--- minichat_post.php
+++ minichat_post.php
@@ -11,16 +11,36 @@ catch(Exception $e)
 }
 
 // Création des variables
-$pseudo = $_POST['pseudo'];
 $message = $_POST['message'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$jour = 7; // Nombre de jour avant suppression du cookie
+
+// Si le cookie n'existe pas crée le pour les fois suivantes
+if(!isset($_COOKIE['pseudo']) || $_POST['pseudo'] !== $_COOKIE['pseudo'])
+{
+    setcookie('pseudo', $_POST['pseudo'], time() + $jour*24*3600, null, null, false, true);
+}
 
 // Insertion du message à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(:pseudo, :message)');
+$req = $bdd->prepare('INSERT INTO minichat(pseudo, message, ip) VALUES(:pseudo, :message, :ip)');
 
+// Si le cookie n'existe pas au premier lancement execute avec le post
+if(isset($_COOKIE['pseudo']))
+{
 $req->execute(array(
-    'pseudo' => $pseudo,
-    'message' => $message
+    'pseudo' => $_COOKIE['pseudo'],
+    'message' => $message,
+    'ip' => $ip
     ));
+}
+else
+{
+$req->execute(array(
+    'pseudo' => $_POST['pseudo'],
+    'message' => $message,
+    'ip' => $ip
+    ));
+}
 
 // Redirige vers l'index
 header('Location: minichat.php');
